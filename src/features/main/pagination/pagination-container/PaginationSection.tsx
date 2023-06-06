be2a5abc4148d147f6c2a2bf796c2206451beca5
Bogdan <romanbs.wo@gmail.com>
2023-06-06 21:24:52 +0300

    s

--- src/features/main/pagination/pagination-container/PaginationSection.tsx
+++ src/features/main/pagination/pagination-container/PaginationSection.tsx
@@ -14,16 +14,24 @@ const PaginationSection: React.FC<PaginationSectionProps> = ({ getPagination }) 
   const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   const itemsPerPage = 5;
 
+  const items2: object = {
+    1: [1, 2, 3, 4, 5],
+    2: [1, 2, 3, 4, 5],
+    3: [1, 2, 3, 4, 5],
+  };
+
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
     <section className="pagination-section">
       <ul className="items-list">
-        {items.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map(item => (
-          <li className="items-list__item" key={item}>
-            Item {item}
-          </li>
-        ))}
+        {items
+          .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+          .map((item, index) => (
+            <li className={`items-list__item ${index < 2 ? 'first-row' : 'second-row'}`} key={item}>
+              <span className="item-text">Item {item}</span>
+            </li>
+          ))}
       </ul>
 
       <PaginationPages currentPage={currentPage} setCurrentPage={setCurrentPage} />
